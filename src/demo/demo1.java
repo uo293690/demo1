1cf7a4d03fae97e3ddc36a3368a07420258b6891
UO293690 <UO293690@DESKTOP-TGTDRCA>
2025-02-17 12:21:07 +0100

    System.out.print("Linea 4 por master");

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -5,6 +5,9 @@ public class demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Linea 1");
+		
+		
+		System.out.print("Linea 4 por master");
 	}
 
 }
